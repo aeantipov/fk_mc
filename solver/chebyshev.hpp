cf38fdf3b491e9904580091b6bd0e3a09c00ba85
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2014-05-04 13:03:50 -0400

    chebyshev routines

--- solver/chebyshev.hpp
+++ solver/chebyshev.hpp
@@ -7,43 +7,50 @@ namespace fk {
 
 inline double chebyshev_t(double x, size_t n)
 {
-    #ifndef NDEBUG
-        if (std::abs(x)>1) throw std::logic_error("Argument " + std::to_string(x) + " of Chebyshev polynomial is not within -1 and 1 bounds");
-    #endif
+    assert(std::abs(x)<=1);
     return cos(n*acos(x));
 }
 
-template <typename F, typename Grid>
-inline double simpson(F&& f, const Grid& v)
+struct chebyshev_eval 
 {
-    int n = v.size();
-    typedef decltype(f(v[0])) value_type; 
-    value_type s=0;
-    assert(n>=8);
-    for (int i=4;i<n-4;i++) { s += 48.*f(v[i]); }
-    auto dx = v[1] - v[0];
-    return dx/48.*(17.*f(v[0]) + 59.*f(v[1]) + 43.*f(v[2])+49.*f(v[3]) + s + 49. *f(v[n-4]) + 43.*f(v[n-3]) + 59.*f(v[n-2]) + 17.*f(v[n-1]));
-}
+    chebyshev_eval(int max_moment, int n):
+        angle_grid(Eigen::VectorXd::LinSpaced(Eigen::Sequential, n, 0., 1.)),
+        lobatto_grid(n),
+        chebt_cache(max_moment, n)
+    {
+        for (int i=0; i<angle_grid.size(); i++) { 
+            //angle_grid[i] = (1.*i/n);//(2.*i + 1.) / (2.*n);
+            lobatto_grid[i] = -cos(M_PI*angle_grid[i]);
+            for (int k=0; k<max_moment; k++) chebt_cache(k,i) = chebyshev_t(lobatto_grid[i], k); 
+            }
+    }
 
-template <typename F, typename Grid>
-inline double chebyshev_moment(const F& op, int order, const Grid& grid)
-{
-    auto ch = std::bind(chebyshev_t, std::placeholders::_1, order);
-    auto f = [&](double w){return 1./M_PI/sqrt(1-w*w)*op(w)*ch(w);};
-    auto d = simpson(f,grid);
-    return d; 
-}
-/*
-template <typename F, typename Grid>
-inline double chebyshev_moment2(const F& op, int order, const Grid& grid)
-{
-    auto ch = std::bind(chebyshev_t, std::placeholders::_1, order);
-    auto f = [&](double w){return 1./M_PI*op(w)*ch(w);};
-    double s = 0.0;
-    for (size_t i =0; i<grid.size(); ++i) s+=f(grid[i]);
-    return s; 
-}*/
+    template <typename F>
+    inline auto moment(const F& op, int order) -> typename std::remove_reference<typename std::result_of<F(double)>::type>::type const { // trapezoidal
+        typedef typename std::remove_reference<typename std::result_of<F(double)>::type>::type value_type;
+        std::vector<value_type> vals(angle_grid.size());
+        for (size_t i =0; i<angle_grid.size(); ++i) vals[i] = op(lobatto_grid[i]);
+        return moment(vals, order); 
+    }
 
+    template <class F>// decltype (std::declval<F>()[0])>
+        auto moment(F &&in, int order) const -> 
+            typename std::remove_reference<decltype (std::declval<F>()[0])>::type {
+        typedef typename std::remove_reference<decltype (std::declval<F>()[0])>::type value_type;
+        value_type s = 0.0;
+        for (size_t i =0; i<angle_grid.size()-1; ++i) { 
+            s+=(in[i+1]*chebt_cache(order, i+1) + in[i]*chebt_cache(order, i))*(angle_grid[i+1]-angle_grid[i]);
+            };
+        return s*0.5; 
+    }
 
+protected:
+    /// Uniform grid between (-1;1) 
+    Eigen::VectorXd angle_grid;
+    /// Lobatto grid (x[i] = -cos(PI*angle_grid[i]))  
+    Eigen::VectorXd lobatto_grid;
+    /// Cache of Chebyshev T polynomials at the points defined by grid
+    Eigen::MatrixXd chebt_cache;
+};
 
 } // end of namespace fk
